6106a417222122a5ebea575b9910e6b508b2ad08
karanmehta1595 <karanmehta1595@gmail.com>
2026-02-18 05:07:12 +0530

    Create Testgit.c

---
+++ Testgit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a=61;
+    float b=52.20;
+    double c=62.1;
+    double Result = a * b * c;
+    printf("Final Result = %.2lf\n", Result);
+    return 0;
+}
